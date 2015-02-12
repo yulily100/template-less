43602fdfbb5d204a0343a9fe3292085c9a9083e3
yulily100 <yulililily100@yahoo.co.jp>
2015-02-12 15:11:24 +0900

    livereloadできない

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,26 @@
-var gulp = require("gulp");
+var gulp,connect;
+    gulp = require('gulp');
+    connect = require('gulp-connect');
+
+
+//ローカルサーバー
+gulp.task('connectDev',function(){
+  connect.server({
+    root: ['./'],   //ルートディレクトリ
+    port: 8000,     //ポート番号
+    livereload: true
+  });
+});
+
+//htmlタスク
+gulp.task('html',function(){
+  gulp.src('./*.html')          //実行するファイル
+    .pipe(connect.reload());    //ブラウザの更新
+});
+
+//ファイルの監視
+gulp.task('watch',function(){
+  gulp.watch(['./*.html'],['html']);    //htmlファイルを監視
+});
+
+gulp.task('default',['connectDev','watch']);
